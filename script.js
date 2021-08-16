62db3f10c6dcd97358aaa96ea28ad6067c7d83a1
Kaushalendra Pandey <pandeysid713@gmail.com>
2021-08-16 22:00:43 +0530

    feat: added calculation logic and table

---
+++ script.js
@@ -0,0 +1,32 @@
+let mainAmount = document.getElementById("main-amount")
+let paidAmount = document.getElementById("paid-amount")
+let alert = document.getElementById("alert-text")
+let denominations = [2000,500,200,100,50,20,10,5,2,1]
+let ans = []
+let tableData = document.querySelectorAll("td")
+console.log(tableData)
+
+
+const formAnswer = (amountToReturn) => {
+    for(let i = 0 ; i<denominations.length; i++) {
+        let numOfNotes = Math.trunc(amountToReturn / denominations[i])
+        amountToReturn %= denominations[i]
+        tableData[i].innerHTML = numOfNotes
+        if(numOfNotes > 0){
+            tableData[i].style.backgroundColor = "aqua"
+        }
+        
+    }    
+}
+
+const solve = () => {
+    alert.innerHTML = ""
+    for(let i = 0; i<tableData.length;i++){
+        tableData[i].style.backgroundColor = "white"
+    }
+    amountToReturn = paidAmount.value - mainAmount.value
+    if(amountToReturn < 0) {
+        alert.innerHTML = "Dhandha aise nhi chalta!"
+    }
+    formAnswer(amountToReturn)
+}
