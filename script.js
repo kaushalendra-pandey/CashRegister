f30fdf38f8faf05e9b350b331e07eaaad63da3ac
Kaushalendra Pandey <pandeysid713@gmail.com>
2021-08-16 22:07:40 +0530

    bug: added scroll to bottom feature

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ const formAnswer = (amountToReturn) => {
     for(let i = 0 ; i<denominations.length; i++) {
         let numOfNotes = Math.trunc(amountToReturn / denominations[i])
         amountToReturn %= denominations[i]
-        tableData[i].innerHTML = numOfNotes
+        
         if(numOfNotes > 0){
+            tableData[i].innerHTML = numOfNotes
             tableData[i].style.backgroundColor = "aqua"
         }
         
@@ -29,4 +30,5 @@ const solve = () => {
         alert.innerHTML = "Dhandha aise nhi chalta!"
     }
     formAnswer(amountToReturn)
+    window.scrollTo(0,document.body.scrollHeight)
 }
